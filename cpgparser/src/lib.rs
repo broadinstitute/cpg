d457d1e3cecf5a9dd8ef91e0dc03c00e23dbcb7a
Ankur Kumar <ank@leoank.me>
2024-01-11 15:26:51 -0500

    Feat(parser): Add rust version of the parser
    Python version of the parser takes more time than the aws lambda timeout
    to process a single inventory file.
    Rust version of the parser is ~10x faster and takes less time than the
    lambda timeout.
    
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ cpgparser/src/lib.rs
@@ -0,0 +1,66 @@
+use pyo3::prelude::{pyfunction, pymodule, wrap_pyfunction, PyModule, PyResult, Python};
+use std::collections::HashMap;
+use std::fmt;
+
+use pest::{Parser, Token};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "key_parser.pest"]
+pub struct KeyParser;
+
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+/// Parse S3 prefixs in Cell painting gallery.
+///
+/// * `prefix`: S3 prefix as a string
+#[pyfunction]
+fn parse_prefix(prefix: String) -> PyResult<HashMap<String, String>> {
+    let mut parsed_prefix: HashMap<String, usize> = HashMap::new();
+    let mut parsed: HashMap<String, String> = HashMap::new();
+    let key = KeyParser::parse(Rule::key, &prefix).unwrap();
+    let key_tokens = key.tokens();
+    for token in key_tokens {
+        match token {
+            Token::Start { rule, pos } => {
+                parsed_prefix.insert(rule.to_string(), pos.pos());
+            }
+            Token::End { rule, pos } => {
+                if let Some((_, v)) = parsed_prefix.remove_entry(&rule.to_string()) {
+                    let value: String = (&prefix[v..pos.pos()]).into();
+                    parsed.insert(rule.to_string(), value);
+                }
+            }
+        }
+    }
+    return Ok(parsed);
+}
+
+#[pyfunction]
+fn parse_prefix_allow_threads(py: Python<'_>, prefix: String) -> PyResult<HashMap<String, String>> {
+    py.allow_threads(|| parse_prefix(prefix))
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn cpgparser(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(parse_prefix, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_prefix_allow_threads, m)?)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_prefix;
+
+    #[test]
+    fn check_smaple_1() {
+        let prefix: String = "cpg0000-jump-pilot/source_4/images/2020_11_04_CPJUMP1/illum/BR00116991/BR00116991_IllumHighZBF.npy".to_string();
+        parse_prefix(prefix);
+        // assert_eq!()
+    }
+}
