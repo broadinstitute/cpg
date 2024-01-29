94a82689fb0d527c8f94bbbcfb43bbb3920fff31
Ankur Kumar <ank@leoank.me>
2024-01-29 18:21:21 -0500

    Chore(aws_wrapper): Add option to run as a file
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- complex/src/complex/aws_wrapper.py
+++ complex/src/complex/aws_wrapper.py
@@ -11,3 +11,7 @@ def awscli() -> None:
     if os.environ.get("LC_CTYPE", "") == "UTF-8":
         os.environ["LC_CTYPE"] = "en_US.UTF-8"
     return sys.exit(clidriver.main())
+
+
+if __name__ == "__main__":
+    sys.exit(clidriver.main())
